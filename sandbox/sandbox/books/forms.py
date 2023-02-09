18753b77b8d5d8fac7bdd8e9f5ecb17b6b51773b
MagiMats <matsjanssens2004@gmail.com>
2023-02-09 12:20:09 +0100

    Tried making the author book model but failed

--- sandbox/sandbox/books/forms.py
+++ sandbox/sandbox/books/forms.py
@@ -1,13 +1,14 @@
 from django.forms import ModelChoiceField, ModelForm
 
-from .models import Book, Author
+from .models import Book
+
+import fitz
 
 class UserChoiceField(ModelChoiceField):
     def label_from_instance(self, obj):
         return obj.name
 
-class BookUploadForm(ModelForm):
-    author = UserChoiceField(Author.objects.all())
+class BookUploadForm(ModelForm):    
     class Meta:
         model = Book
-        fields = ['author', 'title', 'book_file']
+        fields = ['title', 'book_file']
--- sandbox/sandbox/books/models.py
+++ sandbox/sandbox/books/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-
-class Author(models.Model):
-    name = models.CharField(max_length=200)
+import fitz
 
 class Book(models.Model):
-    page_amount = models.IntegerField(default='0')
-    
+    page_amount = models.IntegerField(default='0')    
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
 
     book_file = models.FileField(upload_to='books', null=True)
     file_text = models.TextField(null=True)
